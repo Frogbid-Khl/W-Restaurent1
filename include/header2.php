aeca7b8cccba48a7d68c78cec6a1c2bc8af2866c
Monoget <monoget2@gmail.com>
2023-10-18 12:40:07 +0600

    10/17/2023

--- include/header2.php
+++ include/header2.php
@@ -10,7 +10,9 @@
                 </div>
 
                 <!-- Nav Toggle Button -->
-                <button class="navbar-toggler navbar-toggler navbar-toggler collapsed navicon justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler navbar-toggler navbar-toggler collapsed navicon justify-content-end"
+                        type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span></span>
                     <span></span>
                     <span></span>
@@ -23,7 +25,8 @@
                             <div class="input-group">
                                 <input required="required" type="text" class="form-control" placeholder="Search">
                                 <div class="input-group-addon">
-                                    <button name="submit" value="submit" type="submit" class="btn btn-primary btn-hover-2">
+                                    <button name="submit" value="submit" type="submit"
+                                            class="btn btn-primary btn-hover-2">
                                         <span><i class="fa-solid fa-magnifying-glass"></i></span>
                                     </button>
                                 </div>
@@ -38,27 +41,27 @@
                         <a href="index.php" class="anim-logo"><img src="assets/images/logo.png" alt="/"></a>
                     </div>
                     <ul class="nav navbar-nav navbar navbar-left">
-                        <li class=""><a href="index.php"">Home</a>
-                        </li>
+                        <li><a href="index.php">Home</a></li>
                         <li class="sub-menu-down"><a href="javascript:void(0);"> Restaurant</a>
                             <ul class="sub-menu">
-                                <li><a href="pak-loh.php"> Pak Loh</a></li>
+                                <li><a href="pak-loh.php">Pak Loh</a></li>
                                 <li><a href="moon-lok.php">Moon Lok</a></li>
                             </ul>
                         </li>
                         <li class="sub-menu-down"><a href="javascript:void(0);"> Menu</a>
                             <ul class="sub-menu">
-                                <li><a href="assets/pdf/PLEL%20_%20PLTS%202023%20Menu_2023_05.pdf" target="_blank"> Pak Loh</a></li>
-                                <li><a href="assets/pdf/MLCC%20TC%20_%20MIRA%20Menu%202023_04_rev3.pdf" target="_blank">Moon Lok</a></li>
+                                <li><a href="assets/pdf/PLEL%20_%20PLTS%202023%20Menu_2023_05.pdf" target="_blank"> Pak
+                                        Loh</a></li>
+                                <li><a href="assets/pdf/MLCC%20TC%20_%20MIRA%20Menu%202023_04_rev3.pdf" target="_blank">Moon
+                                        Lok</a></li>
                             </ul>
                         </li>
-                        <li class=""><a href="event.php">
-                                Event & Catering</a>
-                        <li class=""><a href="news.php">News</a>
+                        <li><a href="event.php">Event & Catering</a>
+                        <li><a href="news.php">News</a>
                         </li>
-                        <li class=""><a href="javascript:void(0);">Press </a>
+                        <li><a href="javascript:void(0);">Press </a>
                         </li>
-                        <li class=""><a href="shop.php">Eshop </a>
+                        <li><a href="shop.php">Eshop</a>
                         </li>
                         <li><a href="contact-us.php">Contact Us</a></li>
                     </ul>
